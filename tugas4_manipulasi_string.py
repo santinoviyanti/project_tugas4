c0394b6bc9bf49fb4dfae4182a64eec6f5440fc4
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-11-23 17:21:48 +0700

    manipulasi string (memecah suku kata)

---
+++ tugas4_manipulasi_string.py
@@ -0,0 +1,11 @@
+a = "Universitas Bhayangkara Jakarta Raya"
+b = a.split(" ")
+print(b)
+
+a = "Fakultas Ilmu Komputer/FASILKOM"
+b = a.split("/")
+print(b)
+
+a = "Informatika, Psikologi, Manajemen, Hukum"
+b = a.split(",")
+print(b)
